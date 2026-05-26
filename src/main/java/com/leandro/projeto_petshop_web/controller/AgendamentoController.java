131059ee317b491d96aa28b9b7087ac51f927a80
leandro509 <leandrogabriel509@gmail.com>
2026-05-26 11:22:26 -0300

    att dependency

--- src/main/java/com/leandro/projeto_petshop_web/controller/AgendamentoController.java
+++ src/main/java/com/leandro/projeto_petshop_web/controller/AgendamentoController.java
@@ -1,8 +1,6 @@
 package com.leandro.projeto_petshop_web.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 //Todas as rotas ficam aqui dentro de controllers
 @RestController
 @RequestMapping("/agendamento")
